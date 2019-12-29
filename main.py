7f6706b2f36e42a786ad756ea449fb4b5c84ff47
thanh <thanhbok26b@gmail.com>
2019-12-29 13:24:55 +0700

    Setup and example

---
+++ main.py
@@ -0,0 +1,22 @@
+import gym
+
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.deepq.policies import MlpPolicy
+from stable_baselines import DQN
+
+env = gym.make('Atlantis-ram-v4')
+
+model = DQN(MlpPolicy, env, verbose=3)
+model.learn(total_timesteps=1000000)
+
+observation = env.reset()
+total_reward = 0
+for i in range(18000):
+    action, _states = model.predict(observation)
+    observation, reward, done, info = env.step(action)
+    env.render()
+    total_reward += reward
+    if done:
+        break
+
+print(reward)
